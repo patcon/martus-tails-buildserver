2c8043449679edb10ff48b4b09626aa1d08d6f9b
Patrick Connolly <patrick.c.connolly@gmail.com>
2015-02-22 21:55:48 +0000

    Cleaned up package signing script output.

--- sign-packages.sh
+++ sign-packages.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 
-APT_REPO_DIR=$PWD/live-persistence/apt/deb-repo/oracle-java8
+GIT_ROOT=$PWD
+APT_REPO_DIR=$GIT_ROOT/live-persistence/apt/deb-repo/oracle-java8
 # TODO: allow choice of fingerprint via envvar
 GPG_KEYID=0x2ED437F7FC76AE12
 
 echo "Copying packages to $APT_REPO_DIR..."
-rsync -avz *.deb $APT_REPO_DIR
+rsync -az *.deb $APT_REPO_DIR
 
 echo -n "Checking for at least one GPG key... "
 gpg --with-colons --list-secret-keys | grep "fpr" 1>/dev/null || (echo "No GPG key present. Exiting." && exit 1)
 echo "ok"
 
-echo "Signing deb packages..."
 cd $APT_REPO_DIR
-dpkg-sig -k "$GPG_KEYID" --sign builder *.deb
+
+echo "Signing deb packages..."
+dpkg-sig --batch=1 --g "--batch" -k "$GPG_KEYID" --sign builder *.deb
+echo "ok."
+
+echo -n "Generating signed package and release files... "
 apt-ftparchive packages . > Packages
 gzip --to-stdout Packages > Packages.gz
 apt-ftparchive release . > Release
-gpg --clearsign --output InRelease Release
-gpg --armor --detach-sign --sign --output Release.gpg Release
+gpg --default-key="$GPG_KEYID" --batch --yes --clearsign --output InRelease Release
+gpg --default-key="$GPG_KEYID" --batch --yes --armor --detach-sign --sign --output Release.gpg Release
+echo "ok."
 
 echo -n "Generating trusted keyring... "
-gpg --armor --export "$GPG_KEYID" > $PWD/live-persistence/apt/trusted.gpg.d/_local-repo.gpg
+gpg --armor --export "$GPG_KEYID" > $GIT_ROOT/live-persistence/apt/trusted.gpg.d/_local-repo.gpg
 echo "ok."
